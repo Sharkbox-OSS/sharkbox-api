7be5f630f850c69e0feeeb8fd5e04b625b7aaf87
Andrew Landsverk <dewdew@gmail.com>
2025-02-06 19:18:19 -0600

    Initial Commit - Skeleton Spring Project

---
+++ src/main/java/dev/sharkbox/api/SharkboxApiApplication.java
@@ -0,0 +1,13 @@
+package dev.sharkbox.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkboxApiApplication.class, args);
+	}
+
+}
